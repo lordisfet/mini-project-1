af9e3b7d5892c0f0c37e5d63f82c51f084952cee
lordisfet <lordisfet@gmail.com>
2026-03-08 17:39:09 +0200

    Treshhold and ON/OFF swap

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,46 @@
 #include <Arduino.h>
+#include <Ticker.h>
 
 bool isRelayActive(int value);
+void printRelayStatus(bool status, int value);
 
 const int MONITOR_BAUD_RATE = 115200;
 const int8_t ADC_PIN = 5;
 const int8_t ADC_RESOLUTION = 12;
 constexpr int16_t ADC_MAX_VALUE = (1 << ADC_RESOLUTION) - 1;
 const int8_t RELAY_IN_PIN = 4;
+const int8_t RELAY_STATUS_PRINT_DELAY = 1;
+int rawValue;
+
+Ticker printRelayStatusTicker;
 
 void setup() {
     Serial.begin(MONITOR_BAUD_RATE);
     pinMode(ADC_PIN, INPUT);
     pinMode(RELAY_IN_PIN, OUTPUT);
-
+    
     analogReadResolution(ADC_RESOLUTION);
 
+    printRelayStatusTicker.attach(RELAY_STATUS_PRINT_DELAY, [] () {
+      printRelayStatus(digitalRead(RELAY_IN_PIN), rawValue);
+    });
 }
 
 void loop() {
-  int rawValue = analogRead(ADC_PIN);
+  rawValue = analogRead(ADC_PIN);
   if(isRelayActive(rawValue)) {
-    digitalWrite(RELAY_IN_PIN, HIGH);
-    Serial.println("Relay activated. RAW: " + String(rawValue));
+    digitalWrite(RELAY_IN_PIN, LOW);
   }
   else {
-    digitalWrite(RELAY_IN_PIN, LOW);
-    Serial.println("Relay deactivated. RAW: " + String(rawValue));
+    digitalWrite(RELAY_IN_PIN, HIGH);
   }
-
-  // digitalWrite(RELAY_IN_PIN, LOW);
-  // delay(1000);
-  // digitalWrite(RELAY_IN_PIN, HIGH);
-  // delay(1000);
 }
 
 bool isRelayActive(int value) {
   static bool relayState = false; 
 
-  int upperThreshold = ADC_MAX_VALUE / 2;
-  int lowerThreshold = (ADC_MAX_VALUE - 300) / 2;
+  int upperThreshold = 3 * ADC_MAX_VALUE / 4;
+  int lowerThreshold = upperThreshold - 300;
 
   if (value > upperThreshold) {
     relayState = true;
@@ -48,4 +50,15 @@ bool isRelayActive(int value) {
   }
 
   return relayState;
+}
+
+void printRelayStatus(bool status, int value) {
+  if (status) 
+  {
+    Serial.println("Relay " + String(status) + ". RAW: " + String(value));
+  }
+  else
+  {
+    Serial.println("Relay " + String(status) + ". RAW: " + String(value));
+  }
 }
